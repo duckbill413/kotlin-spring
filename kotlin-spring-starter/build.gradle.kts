38b77021aa897ab9ca4297e4d5cdca99fd1cb49f
duckbill413 <uhyeon7399@daum.net>
2024-09-02 23:34:40 +0900

    자바 기반 스프링 프로젝트에 코틀린 적용 - plugin.allopen - plugin.jpa

--- kotlin-spring-starter/build.gradle.kts
+++ kotlin-spring-starter/build.gradle.kts
@@ -1,9 +1,25 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.allopen") version "1.9.25" // all-open
+	// kotlin("plugin.noarg") version "1.9.25" // no-argument plugin
+	kotlin("plugin.jpa") version "1.9.25" //
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 }
+
+// allopen 을 사용하면 에노테이션이 붙어 있는 클래스에 대해서는 모두 다 allopen 상태가 됩니다.
+// kotlin spring 에서는 자동으로 all-open 처리가 된다.
+//allOpen {
+//	annotations("org.springframework.boot.autoconfigure.SpringBootApplication")
+//}
+
+// plugin.noarg 를 사용하는 경우에는 annotation 에 path를 정의할 필요가 있다.
+// jpa 를 사용하는 경우 이러한 점을 불편
+// 이를 대체하기 위해서 plugin.jpa plugin을 사용할 수 있다.
+//noArg {
+//	annotations("package-path")
+//}
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
