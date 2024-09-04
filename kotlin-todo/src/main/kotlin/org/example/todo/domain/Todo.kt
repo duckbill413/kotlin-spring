8763023ddd01be97628279849871b1b218d25d37
duckbill413 <uhyeon7399@daum.net>
2024-09-04 23:50:22 +0900

    TODO Domain Entity 생성

---
+++ kotlin-todo/src/main/kotlin/org/example/todo/domain/Todo.kt
@@ -0,0 +1,30 @@
+package org.example.todo.domain
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "todos")
+class Todo(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = 0,
+    @Column(name = "title")
+    var title: String,
+    @Lob
+    @Column(name = "description")
+    var description: String,
+    @Column(name = "done")
+    var done: Boolean,
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime,
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime
+) {
+    fun update(title: String, description: String, done: Boolean) {
+        this.title = title
+        this.description = description
+        this.done = done
+        this.updatedAt = LocalDateTime.now()
+    }
+}
