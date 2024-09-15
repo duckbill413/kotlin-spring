5644e1a4a7333fcd59325bfc3d8f02d763f7146a
duckbill413 <uhyeon7399@daum.net>
2024-09-15 15:12:46 +0900

    이슈 수정 API

--- kotlin-jara/issue-service/src/main/kotlin/wh/duckbill/issue/service/IssueService.kt
+++ kotlin-jara/issue-service/src/main/kotlin/wh/duckbill/issue/service/IssueService.kt
@@ -39,4 +39,19 @@ class IssueService(
             issueRepository.findByIdOrNull(id)
                 ?: throw NotFoundException("이슈가 존재하지 않습니다.")
         )
+
+    @Transactional
+    fun edit(userId: Long, id: Long, request: IssueRequest): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다.")
+
+        return with(issue) {
+            summary = request.summary
+            description = request.description
+            this.userId = userId
+            type = request.type
+            priority = request.priority
+            status = request.status
+            IssueResponse(issueRepository.save(this))
+        }
+    }
 }
