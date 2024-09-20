a8cfa7d5df594df89255fccfe16bb1e88baedb7d
duckbill413 <uhyeon7399@daum.net>
2024-09-20 19:52:06 +0900

    함수형 엔드포인트

--- kotlin-webflux/src/main/kotlin/wh/duckbill/webflux/Router.kt
+++ kotlin-webflux/src/main/kotlin/wh/duckbill/webflux/Router.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class Router {
@@ -15,4 +16,17 @@ class Router {
             .GET("/", handler::sayHello)
             .build()
 
+    @Bean
+    fun userRouter(handler: UserHandler): RouterFunction<ServerResponse> =
+//        route()
+//            .GET("/users/{id}", handler::getUser)
+//            .GET("/users", handler::getAll)
+//            .build()
+        // 중첩라이팅으로 변경
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
 }
