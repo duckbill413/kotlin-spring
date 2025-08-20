412e19b5484538431414d598d510d3778bf4a5bd
duckbill413 <uhyeon7399@daum.net>
2025-08-21 01:58:48 +0900

    14. LAZY 를 사용하여 실행 주기 관리하기

---
+++ coroutine master/src/main/kotlin/동시성/BASE_2/Lazy 를 사용하여 실행 주기관리하기.kt
@@ -0,0 +1,32 @@
+package org.coroutine.동시성.BASE_2
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+suspend fun one() : Int {
+  delay(1000L)
+  return 13
+}
+
+suspend fun two() : Int {
+  delay(1000L)
+  return 29
+}
+
+/**
+ * LAZY 를 이용하여 명시적으로 실행 시점을 제어
+ */
+fun main() = runBlocking {
+  val time = measureTimeMillis {
+    val one = async(start=CoroutineStart.LAZY) { one() }
+    val two = async(start=CoroutineStart.LAZY) { two() }
+    one.start()
+    two.start()
+    println("one + two = ${one.await() + two.await()}")
+  }
+  println("Completed in $time ms")
+}
