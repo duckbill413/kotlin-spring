67d2ca8990e0a7155a08af0a9d24120c3f861284
duckbill413 <uhyeon7399@daum.net>
2025-09-13 22:16:00 +0900

    은행서버 프로젝트 실습을 통해 배우는 코틀린 마스터 클래스

---
+++ kotlin-bank/src/main/kotlin/wh/duckbill/bank/Main.kt
@@ -0,0 +1,11 @@
+package wh.duckbill.kotlin.wh.duckbill.kotlin_bank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
