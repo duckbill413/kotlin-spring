b001c598fbdc98c351af78deedf0271a4533043c
duckbill413 <uhyeon7399@daum.net>
2025-09-22 23:25:00 +0900

    OAuth2 Call 구현 및 싱글톤 활용하기

--- kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/auth/service/GithubAuthService.kt
+++ kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/auth/service/GithubAuthService.kt
@@ -1,9 +1,10 @@
 package wh.duckbill.bank.domains.auth.service
 
-import org.apache.naming.ResourceRef.AUTH
+import okhttp3.FormBody
 import org.springframework.stereotype.Service
 import wh.duckbill.bank.common.exception.CustomException
 import wh.duckbill.bank.common.exception.ErrorCode
+import wh.duckbill.bank.common.httpClient.CallClient
 import wh.duckbill.bank.config.OAuth2Config
 import wh.duckbill.bank.interfaces.OAuth2TokenResponse
 import wh.duckbill.bank.interfaces.OAuth2UserResponse
@@ -13,13 +14,26 @@ private const val key = "github"
 
 @Service(key)
 class GithubAuthService(
+  private val httpClient: CallClient,
   private val config: OAuth2Config
 ) : OAuthServiceInterface {
   private val oAuthInfo = config.providers[key] ?: throw CustomException(ErrorCode.AUTH_CONFIG_NOT_FOUND, key)
+  private val tokenURL = "https://github.com/login/oauth/access_token"
+  private val userInfoURL = "https://api.github.com/user"
 
   override val providerName: String = key
 
   override fun getToken(code: String): OAuth2TokenResponse {
+    val body = FormBody.Builder()
+      .add("code", code)
+      .add("client_id", oAuthInfo.clientId)
+      .add("client_secret", oAuthInfo.clientSecret)
+      .add("redirect_uri", oAuthInfo.redirectUri)
+      .add("grant_type", "authorization_code")
+      .build()
+
+    val headers = mapOf("Accept" to "application/json")
+    val jsonString = httpClient.POST("", headers, body)
     TODO("Not yet implemented")
   }
 
