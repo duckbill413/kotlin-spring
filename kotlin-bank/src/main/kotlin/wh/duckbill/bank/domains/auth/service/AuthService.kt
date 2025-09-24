9c9fce7fa5d4c7b17d7c2d794e6186652ddae6db
duckbill413 <uhyeon7399@daum.net>
2025-09-24 23:58:28 +0900

    OAuth2 Service 작업 및 JWT

--- kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/auth/service/AuthService.kt
+++ kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/auth/service/AuthService.kt
@@ -1,4 +1,10 @@
 package wh.duckbill.bank.domains.auth.service
 
-class AuthService {
+import org.springframework.stereotype.Service
+import wh.duckbill.bank.interfaces.OAuthServiceInterface
+
+@Service
+class AuthService(
+  private val oAuthServices: Map<String, OAuthServiceInterface>
+) {
 }
