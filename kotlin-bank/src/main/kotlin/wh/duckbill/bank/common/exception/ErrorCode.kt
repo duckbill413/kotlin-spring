fdae99faffad2ea5b657decc0706fa826b6a01b7
duckbill413 <uhyeon7399@daum.net>
2025-10-04 22:16:00 +0900

    23. logic 마무리 및 동시성에 대한 고찰

--- kotlin-bank/src/main/kotlin/wh/duckbill/bank/common/exception/ErrorCode.kt
+++ kotlin-bank/src/main/kotlin/wh/duckbill/bank/common/exception/ErrorCode.kt
@@ -17,5 +17,9 @@ enum class ErrorCode(
   TOKEN_IS_INVALID(-104, "token is invalid"),
   TOKEN_IS_EXPIRED(-105, "token is expired"),
   FAILED_TO_INVOKE_IN_LOGGER(-106, "failed to invoke"),
+  FAILED_TO_SAVE_DATA(-107, "failed to save data"),
+  FAILED_TO_FIND_ACCOUNT(-108, "failed to find account"),
+  MISS_MATCH_ACCOUNT_ULID_AND_USER_ULID(-109, "miss match account ulid and user ulid"),
+  ACCOUNT_BALANCE_IS_NOT_ZERO(-110, "account balance is not zero"),
   ;
 }
--- kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/bank/service/BankService.kt
+++ kotlin-bank/src/main/kotlin/wh/duckbill/bank/domains/bank/service/BankService.kt
@@ -1,13 +1,19 @@
 package wh.duckbill.bank.domains.bank.service
 
+import com.github.f4b6a3.ulid.UlidCreator
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
+import wh.duckbill.bank.common.exception.CustomException
+import wh.duckbill.bank.common.exception.ErrorCode
 import wh.duckbill.bank.common.logging.Logging
 import wh.duckbill.bank.common.transaction.Transactional
 import wh.duckbill.bank.domains.bank.repository.BankAccountRepository
 import wh.duckbill.bank.domains.bank.repository.BankUserRepository
 import wh.duckbill.bank.types.dto.Response
+import wh.duckbill.bank.types.dto.ResponseProvider
+import wh.duckbill.bank.types.entity.Account
 import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @Service
 class BankService(
@@ -18,18 +24,75 @@ class BankService(
 ) {
   fun createAccount(userUlid: String): Response<String> = Logging.logFor(logger) { log ->
     log["userUlid"] = userUlid
-    transactional.run {}
+    transactional.run {
+      val user = bankUserRepository.findByUlid(userUlid)
+      val ulid = UlidCreator.getUlid().toString()
+      val accountNumber = generateRandomAccountNumber()
+
+      val account: Account = Account(
+        ulid = ulid,
+        user = user,
+        accountNumber = accountNumber,
+      )
+
+      try {
+        bankAccountRepository.save(account)
+      } catch (e: Exception) {
+        throw CustomException(ErrorCode.FAILED_TO_SAVE_DATA, e.message)
+      }
+    }
+
+    return@logFor ResponseProvider.success("SUCCESS")
   }
 
   fun balance(userUlid: String, accountUlid: String): Response<BigDecimal> = Logging.logFor(logger) { log ->
     log["userUlid"] = userUlid
     log["accountUlid"] = accountUlid
-    transactional.run {}
+
+    // TODO -> 동시성 처리 고려
+
+    return@logFor transactional.run {
+      val account =
+        bankAccountRepository.findByUlid(accountUlid) ?: throw CustomException(ErrorCode.FAILED_TO_FIND_ACCOUNT)
+      if (account.user.ulid != userUlid) {
+        throw CustomException(ErrorCode.MISS_MATCH_ACCOUNT_ULID_AND_USER_ULID)
+      }
+
+      ResponseProvider.success(account.balance)
+    }
   }
 
   fun removeAccount(userUlid: String, accountUlid: String): Response<String> = Logging.logFor(logger) { log ->
     log["userUlid"] = userUlid
     log["accountUlid"] = accountUlid
-    transactional.run {}
+
+    transactional.run {
+      val user = bankUserRepository.findByUlid(userUlid)
+      val account =
+        bankAccountRepository.findByUlid(accountUlid) ?: throw CustomException(ErrorCode.FAILED_TO_FIND_ACCOUNT)
+      if (account.user.ulid != user.ulid) {
+        throw CustomException(ErrorCode.MISS_MATCH_ACCOUNT_ULID_AND_USER_ULID)
+      }
+      if (account.balance.compareTo(BigDecimal.ZERO) != 0) {
+        throw CustomException(ErrorCode.ACCOUNT_BALANCE_IS_NOT_ZERO, account.balance.toString())
+      }
+
+      val updatedAccount = account.copy(
+        isDeleted = true,
+        updatedAt = LocalDateTime.now(),
+      )
+
+      bankAccountRepository.save(updatedAccount)
+      ResponseProvider.success("SUCCESS")
+    }
+    return@logFor ResponseProvider.success("SUCCESS")
+  }
+
+  private fun generateRandomAccountNumber(): String {
+    val bankCode = "003"
+    val section = "12"
+
+    val number = Math.random().toString()
+    return "$bankCode-$section-$number"
   }
 }
